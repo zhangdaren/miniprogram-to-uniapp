03c2a411318f9f6bf902ab2cf9e422e408ffa6c3
qimengjie <qimengjie@163.com>
2023-08-15 15:10:53 +0800

    fix: setData使用$set进行响应式更新

--- src/project/template/uni_modules/zp-mixins/utils/_set.js
+++ src/project/template/uni_modules/zp-mixins/utils/_set.js
@@ -9,7 +9,7 @@ function _set(obj, path, value) {
 	if (Object(obj) !== obj) return obj // When obj is not an object
 	// If not yet an array, get the keys from the string-path
 	if (!Array.isArray(path)) path = path.toString().match(/[^.[\]]+/g) || []
-	path.slice(0, -1).reduce((a, c, i) => // Iterate all of them except the last one
+	const targetObject = path.slice(0, -1).reduce((a, c, i) => // Iterate all of them except the last one
 		Object(a[c]) === a[c] // Does the key exist and is its value an object?
 		// Yes: then follow that path
 		?
@@ -19,7 +19,15 @@ function _set(obj, path, value) {
 		a[c] = Math.abs(path[i + 1]) >> 0 === +path[i + 1] ? [] // Yes: assign a new array object
 		:
 		{}, // No: assign a new plain object
-		obj)[path[path.length - 1]] = value // Finally assign the value to the last key
+		obj)
+	const targetKey = path[path.length - 1]
+	// Finally assign the value to the last key
+	if (typeof obj.$set === 'function') {
+		// 对data中的数组或对象进行修改时，有些操作方式是非响应式的，这里使用$set进行响应式的数据更新
+		obj.$set(targetObject, targetKey, value)
+	} else {
+		targetObject[targetKey] = value
+	}
 	return obj // Return the top-level object to allow chaining
 }
 
